ec14cbe2bdf1e4097a32379649f2934bf3fb857e
jrbaquero <jrbaquero@hotmail.com>
2018-06-19 18:56:24 -0500

    Se agrego el detalle de ventas, solamente se visualista en un listView, se lo debe poner en un Recycler view, de manera que luego se pueda ver el detalle completo, se agrego algunas strings que tenian warnings

--- app/src/main/java/com/example/usuario/pedidos_01/fragmentos/DetalleVentasFragment.java
+++ app/src/main/java/com/example/usuario/pedidos_01/fragmentos/DetalleVentasFragment.java
@@ -32,16 +32,20 @@ public class DetalleVentasFragment extends Fragment {
     SQLiteDatabase sq;
 
     int Mes;
-    TextView TxtMesSelec;
+    TextView TxtMesSelec, TxtDetalle,TxtTotal;
     Float totalMes,totalAnio;
-    String MesSelec,cliente;
+    String MesSelec,cliente,Detalle;
+    int MesSelecDecimal;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_detalle_ventas, container, false);
-        Log.i("TAG", "onClick: Ingresa Detalle VENTAS");
 
+        Log.i("TAG", "\nonClick: Ingresa Detalle VENTAS");
+
+        TxtDetalle = (TextView) view.findViewById(R.id.txt_detalle);
+        TxtTotal =(TextView) view.findViewById(R.id.txt_total);
 
         basedatos = new BaseDatos(getActivity());
         sq = basedatos.getWritableDatabase();
@@ -49,35 +53,47 @@ public class DetalleVentasFragment extends Fragment {
         totalAnio=0f;
 
         Bundle bundle = getArguments();
-//        MesSelec = bundle.getString("Mes");
+        MesSelec = bundle.getString("Mes");
+        MesSelecDecimal=bundle.getInt("MesSelec");
 
         TxtMesSelec = (TextView) view.findViewById(R.id.txt_mesSelec);
 
-        TxtMesSelec.setText("Enero");
 
+        TxtMesSelec.setText(MesSelec);
+        crearTablaVentas(MesSelecDecimal);
+         Log.i("TAG", "Total Mes:"+"AÑO  valor:"+totalAnio+ " mes:"+MesSelec+" "+6);
+        Log.i("TAG", "************************************************");
+        TxtDetalle.setText(Detalle);
+        TxtTotal.setText("Total: "+Float.toString(totalAnio));
       return view;
     }
 
 
 
     private void crearTablaVentas(int mes) {
-
+        Log.i("TAG", "************************************************");
+        Log.i("TAG", "**********Ingresa Crear Tabla ventas **********************");
         Cursor c = sq.rawQuery("SELECT * FROM "+ Estructura.EstructuraBase.TABLE_PEDIDOS+" WHERE "
                 +Estructura.EstructuraBase.COLUMNA_ENREGADO+" = ?  AND "
                 +Estructura.EstructuraBase.COLUMNA_MES+"=?;", new String[] {"1",Integer.toString(mes)});
         int tamano=0;
         totalMes=0f;
-
+        totalAnio=0f;
+        Detalle="";
         if(c.moveToFirst()) {
             do {
 
-                totalMes =c.getFloat(c.getColumnIndex(Estructura.EstructuraBase.COLUMNA_PRECIO))+totalMes;
-                Log.i("TAG", "MES "+mes+" valor:"+totalMes+" Cliente "+cliente);
+                totalMes =c.getFloat(c.getColumnIndex(Estructura.EstructuraBase.COLUMNA_PRECIO));
+                cliente = c.getString(c.getColumnIndex(Estructura.EstructuraBase.COLUMNA_CLIENTE));
+                totalAnio=totalMes+totalAnio;
                 tamano++;
+               Detalle = Integer.toString(tamano)+"\t"+cliente + "\t\t\t$"+Float.toString(totalMes)+"\n" +Detalle;
+                Log.i("TAG", "\n"+tamano+" valor MES:"+totalMes+" valor Año:"+totalAnio+" Cliente "+cliente);
+
             } while (c.moveToNext());
         }
-            totalAnio=totalMes+totalAnio;
-        Log.i("TAG", "AÑO  valor:"+totalAnio);
+
+
 
     }
 
--- app/src/main/java/com/example/usuario/pedidos_01/fragmentos/VentasFragment.java
+++ app/src/main/java/com/example/usuario/pedidos_01/fragmentos/VentasFragment.java
@@ -1,7 +1,6 @@
 package com.example.usuario.pedidos_01.fragmentos;
 
 
-
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -31,11 +30,14 @@ public class VentasFragment extends Fragment {
     NumberFormat format;
     BaseDatos basedatos;
     SQLiteDatabase sq;
-    TextView txtEnero,txtFebrero,txtMarzo,txtAbril,txtMayo,txtJunio,txtTotal,txtEneroMes;
-    TextView txtJulio,txtAgosto,txtSeptiembre,txtOctubre,txtNoviembre,txtDiciembre;
+    TextView txtEnero, txtFebrero, txtMarzo, txtAbril, txtMayo, txtJunio, txtTotal;
+    TextView txtEneroMes, txtFebreroMes, txtMarzoMes, txtAbrilMes, txtMayoMes, txtJunioMes;
+    TextView txtJulioMes, txtAgostoMes, txtSeptiembreMes, txtOctubreMes, txtNoviembreMes, txtDiciembreMes;
+    TextView txtJulio, txtAgosto, txtSeptiembre, txtOctubre, txtNoviembre, txtDiciembre;
     int Mes;
-    Float totalMes,totalAnio;
+    Float totalMes, totalAnio;
     String cliente;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
@@ -59,75 +61,174 @@ public class VentasFragment extends Fragment {
         txtTotal = (TextView) view.findViewById(R.id.txt_total);
 
         txtEneroMes = (TextView) view.findViewById(R.id.txt_enero_mes);
+        txtFebreroMes = (TextView) view.findViewById(R.id.txt_febrero_mes);
+        txtMarzoMes = (TextView) view.findViewById(R.id.txt_marzo_mes);
+        txtAbrilMes = (TextView) view.findViewById(R.id.txt_abril_mes);
+        txtMayoMes = (TextView) view.findViewById(R.id.txt_mayo_mes);
+        txtJunioMes = (TextView) view.findViewById(R.id.txt_junio_mes);
+        txtJulioMes = (TextView) view.findViewById(R.id.txt_julio_mes);
+        txtAgostoMes = (TextView) view.findViewById(R.id.txt_agosto_mes);
+        txtSeptiembreMes = (TextView) view.findViewById(R.id.txt_septiembre_mes);
+        txtOctubreMes = (TextView) view.findViewById(R.id.txt_octubre_mes);
+        txtNoviembreMes = (TextView) view.findViewById(R.id.txt_noviembre_mes);
+        txtDiciembreMes = (TextView) view.findViewById(R.id.txt_diciembre_mes);
 
         basedatos = new BaseDatos(getActivity());
         sq = basedatos.getWritableDatabase();
-        Mes =0;
-        totalAnio=0f;
+        Mes = 0;
+        totalAnio = 0f;
         do {
             crearTablaVentas(Mes);
             escribeValoresLayout(Mes);
 
             Mes++;
-        }while (Mes!=12);
-        if(Mes==12){
+        } while (Mes != 12);
+        if (Mes == 12) {
             txtTotal.setText(format.format(totalAnio));
-            arreglo_TotalMes=null;
+            arreglo_TotalMes = null;
         }
 
         txtEneroMes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Fragment fragment = null;
-                Bundle bundle = new Bundle();
-                bundle.putString("Mes","Enero");
-//                fragment.setArguments(bundle);
-                fragment = new DetalleVentasFragment();
-                getFragmentManager().beginTransaction().replace(R.id.content_segunda, fragment).commit();
+                mesSeleccionado(v.getResources().getString(R.string.mes01),0);
+
+            }
+        });
+        txtFebreroMes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mesSeleccionado(v.getResources().getString(R.string.mes02),1);
+
+
+            }
+        });
+        txtMarzoMes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mesSeleccionado(v.getResources().getString(R.string.mes03),2);
+
+
+            }
+        });
+        txtAbrilMes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mesSeleccionado(v.getResources().getString(R.string.mes04),3);
+
+            }
+        });
+        txtMayoMes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mesSeleccionado(v.getResources().getString(R.string.mes05),4);
+
+            }
+        });
+        txtJunioMes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mesSeleccionado(v.getResources().getString(R.string.mes06),5);
+
+            }
+        });
+        txtJulioMes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mesSeleccionado(v.getResources().getString(R.string.mes07),6);
+
+            }
+        });
+        txtAgostoMes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mesSeleccionado(v.getResources().getString(R.string.mes08),7);
+
+
+            }
+        });
+        txtSeptiembreMes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mesSeleccionado(v.getResources().getString(R.string.mes09),8);
+
+
+            }
+        });
+        txtOctubreMes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mesSeleccionado(v.getResources().getString(R.string.mes10),9);
+
+
+            }
+        });
+        txtNoviembreMes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mesSeleccionado(v.getResources().getString(R.string.mes11),10);
+
+
+            }
+        });
+        txtDiciembreMes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mesSeleccionado(v.getResources().getString(R.string.mes12),11);
+
 
             }
         });
 
 
-      return view;
+        return view;
     }
 
+    private void mesSeleccionado(String MesTxt, int MesSelec){
+        Fragment fragment = null;
+        Bundle bundle = new Bundle();
+        bundle.putString("Mes", MesTxt);
+        bundle.putInt("MesSelec", MesSelec);
+        fragment = new DetalleVentasFragment();
+        fragment.setArguments(bundle);
+        getFragmentManager().beginTransaction().replace(R.id.content_segunda, fragment).commit();
+    }
     private void escribeValoresLayout(int i) {
-        switch (i){
-            case  0:
+        switch (i) {
+            case 0:
                 txtEnero.setText(format.format(totalMes));
                 break;
-            case  1:
+            case 1:
                 txtFebrero.setText(format.format(totalMes));
                 break;
-            case  2:
+            case 2:
                 txtMarzo.setText(format.format(totalMes));
                 break;
-            case  3:
+            case 3:
                 txtAbril.setText(format.format(totalMes));
                 break;
-            case  4:
+            case 4:
                 txtMayo.setText(format.format(totalMes));
                 break;
-            case  5:
+            case 5:
                 txtJunio.setText(format.format(totalMes));
                 break;
-            case  6:
+            case 6:
                 txtJulio.setText(format.format(totalMes));
                 break;
-            case  7:
+            case 7:
                 txtAgosto.setText(format.format(totalMes));
                 break;
-            case  8:
+            case 8:
                 txtSeptiembre.setText(format.format(totalMes));
                 break;
-            case  9:
+            case 9:
                 txtOctubre.setText(format.format(totalMes));
                 break;
-            case  10:
+            case 10:
                 txtNoviembre.setText(format.format(totalMes));
                 break;
-            case  11:
+            case 11:
                 txtNoviembre.setText(format.format(totalMes));
                 break;
         }
@@ -135,26 +236,24 @@ public class VentasFragment extends Fragment {
 
     private void crearTablaVentas(int mes) {
 
-        Cursor c = sq.rawQuery("SELECT * FROM "+ Estructura.EstructuraBase.TABLE_PEDIDOS+" WHERE "
-                +Estructura.EstructuraBase.COLUMNA_ENREGADO+" = ?  AND "
-                +Estructura.EstructuraBase.COLUMNA_MES+"=?;", new String[] {"1",Integer.toString(mes)});
-        int tamano=0;
-        totalMes=0f;
+        Cursor c = sq.rawQuery("SELECT * FROM " + Estructura.EstructuraBase.TABLE_PEDIDOS + " WHERE "
+                + Estructura.EstructuraBase.COLUMNA_ENREGADO + " = ?  AND "
+                + Estructura.EstructuraBase.COLUMNA_MES + "=?;", new String[]{"1", Integer.toString(mes)});
+        int tamano = 0;
+        totalMes = 0f;
 
-        if(c.moveToFirst()) {
+        if (c.moveToFirst()) {
             do {
 
-                totalMes =c.getFloat(c.getColumnIndex(Estructura.EstructuraBase.COLUMNA_PRECIO))+totalMes;
-                Log.i("TAG", "MES "+mes+" valor:"+totalMes+" Cliente "+cliente);
+                totalMes = c.getFloat(c.getColumnIndex(Estructura.EstructuraBase.COLUMNA_PRECIO)) + totalMes;
+                Log.i("TAG", "MES " + mes + " valor:" + totalMes + " Cliente " + cliente);
                 tamano++;
             } while (c.moveToNext());
         }
-            totalAnio=totalMes+totalAnio;
-        Log.i("TAG", "AÑO  valor:"+totalAnio);
+        totalAnio = totalMes + totalAnio;
+        Log.i("TAG", "AÑO  valor:" + totalAnio);
 
     }
-
-
 
 
 }
